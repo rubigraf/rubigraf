dd13468abc6d61e28c7cb713e37865b67bb74f9d
mojtaba5858 <mojtaba.zebardast1384@gmail.com>
2025-09-07 20:02:06 +0330

    added poll status type

---
+++ src/types/pollStatus.ts
@@ -0,0 +1,31 @@
+/**
+ * Status / runtime info for a poll.
+ *
+ * @package rubigraf
+ * @since v1.0.0
+ */
+interface PollStatus {
+  /** Poll state */
+  state: PollStatusEnum;
+  /** Index of selected option (1-based) or 0 meaning not selected */
+  selection_index: number;
+  /** Percent votes for each option */
+  percent_vote_options: number[];
+  /** Total number of votes */
+  total_vote: number;
+  /** Whether to show total votes */
+  show_total_votes: boolean;
+}
+
+/**
+ * Poll status enum.
+ *
+ * @package rubigraf
+ * @since v1.0.0
+ */
+enum PollStatusEnum {
+  Stopped,
+  Live,
+}
+
+export type { PollStatus, PollStatusEnum };
