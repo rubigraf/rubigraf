43da3ecec0c89641fd96d2d0429d7682df695ce3
mojtaba5858 <mojtaba.zebardast1384@gmail.com>
2025-09-07 20:15:43 +0330

    added location type

---
+++ src/types/location.ts
@@ -0,0 +1,14 @@
+/**
+ * Geographic location.
+ *
+ * @package rubigraf
+ * @since v1.0.0
+ */
+interface Location {
+  /** Longitude of the location */
+  longitude: string;
+  /** Latitude of the location */
+  latitude: string;
+}
+
+export type { Location };
