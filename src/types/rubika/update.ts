e80f4f67ce06df78b5ec8d8b4d99cdaa42901a72
mojtaba5858 <mojtaba.zebardast1384@gmail.com>
2025-09-11 21:44:36 +0330

    feat: added update map type for more type safety

--- src/types/rubika/update.ts
+++ src/types/rubika/update.ts
@@ -39,4 +39,16 @@ type Update =
       updated_payment: PaymentStatus;
     });
 
-export type { BaseUpdate, Update };
+/**
+ * Map each UpdateTypeEnum to its corresponding Update subtype.
+ *
+ * This allows `Context<T>` to strongly infer the right fields.
+ *
+ * @package rubigraf
+ * @since v1.0.0
+ */
+type UpdateMap = {
+  [K in Update as K["type"]]: Extract<Update, { type: K["type"] }>;
+};
+
+export type { BaseUpdate, Update, UpdateMap };
