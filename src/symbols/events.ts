0fabd78549dbf97e86f242e885a35107f0d4b315
mojtaba5858 <mojtaba.zebardast1384@gmail.com>
2025-09-13 05:21:47 +0330

    feat: added events symbols

---
+++ src/symbols/events.ts
@@ -0,0 +1,41 @@
+import {
+  Context,
+  Message,
+  NewMessageUpdate,
+  PaymentStatus,
+  RemovedMessageUpdate,
+  StartedBotUpdate,
+  StoppedBotUpdate,
+  Update,
+  UpdatedMessageUpdate,
+  UpdatedPaymentUpdate,
+} from "../types";
+
+namespace RubigrafEvents {
+  export const Error = Symbol("error");
+  export const NewMessage = Symbol("new_message");
+  export const RemovedMessage = Symbol("removed_message");
+  export const StartedBot = Symbol("started_bot");
+  export const StoppedBot = Symbol("stopped_bot");
+  export const UpdatedPayment = Symbol("updated_payment");
+  export const UpdatedMessage = Symbol("updated_message");
+  export const Update = Symbol("update");
+
+  export type Map = {
+    [Error]: [err: Error | string | unknown, code?: number];
+    [NewMessage]: [ctx: Context<NewMessageUpdate>, msg: Message];
+    [RemovedMessage]: [ctx: Context<RemovedMessageUpdate>, id: string];
+    [StartedBot]: [ctx: Context<StartedBotUpdate>, update: Update];
+    [StoppedBot]: [ctx: Context<StoppedBotUpdate>, update: Update];
+    [UpdatedPayment]: [ctx: Context<UpdatedPaymentUpdate>, payment: PaymentStatus];
+    [UpdatedMessage]: [ctx: Context<UpdatedMessageUpdate>, msg: Message];
+    [Update]: [ctx: Context, update: Update];
+  };
+
+  /**
+   * A lazy-loadable event listener wrapper.
+   */
+  export type BotEventInstaller<C, K extends keyof Map> = (events: C) => (...args: Map[K]) => void;
+}
+
+export { RubigrafEvents };
